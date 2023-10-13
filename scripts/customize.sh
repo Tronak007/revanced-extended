dc74e469cec54566554487062156dd3f86b5794a
MatadorProBr <71022656+MatadorProBr@users.noreply.github.com>
2023-10-13 19:24:18 -0300

    Remove YT-Advanced as credits

--- scripts/customize.sh
+++ scripts/customize.sh
@@ -116,7 +116,6 @@ ui_print "* Done"
 ui_print "  by MatadorProBr (github.com/MatadorProBr)"
 ui_print "  thanks to ReVanced Team (github.com/ReVanced)"
 ui_print "  thanks to inotia00 (github.com/inotia00)"
-ui_print "  thanks to YT-Advanced (github.com/YT-Advanced)"
 ui_print "  thanks to j-hc (github.com/j-hc)"
 ui_print " "
 
